9dae58d4cb9971e88a39f7ad819f17cc8cc8ccb3
nathansmith <nathan2012smith@gmail.com>
2023-10-22 19:36:00 -0500

    Completed Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,24 @@
+let history = []
+
 document.addEventListener("DOMContentLoaded", (event)=>{
     const squares = document.querySelectorAll("#board div")
     squares.forEach(square => {
         square.classList.add("square")
+        square.addEventListener("click", (event)=>{
+            if (square.textContent != ""){
+                return
+            }
+            if (history.length == 0 || history[history.length - 1]=="O"){
+                square.textContent = "X"
+                history.push("X")
+            }
+            else
+            {
+                square.textContent = "O"
+                history.push("O")
+            }
+        })
     })
 })
 
+const squares = document.querySelectorAll("#board div")
